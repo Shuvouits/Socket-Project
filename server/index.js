04e44567ee2156ce52b4e20583d151d3db3ea12c
Shuvouits <shuvobhowmik_cse11uits@yahoo.com>
2024-11-08 22:19:30 +0600

    part8 custom logger

--- server/index.js
+++ server/index.js
@@ -7,6 +7,39 @@ dotenv.config();
 
 const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, () => {
+let server;
+
+server = app.listen(PORT, () => {
     logger.info(`server is listening at ${PORT}`);
+    console.log("process id", process.pid);
 });
+
+//handle server error
+
+const exitHandler = () => {
+    if (server){
+        logger.info("server closed");
+    }else{
+        process.exit(1);
+    }
+};
+
+const unexpectedErrorHandler = (error)=> {
+    logger.error(error);
+    process.exit(1);
+    exitHandler();
+}
+
+process.on("uncaughtException", unexpectedErrorHandler);
+process.on("unhandledRejection", unexpectedErrorHandler);
+
+
+
+//SigalTerm comes form signal Termination
+
+process.on("SIGTERM", ()=> {
+    if(server){
+        logger.info('Server Closed');
+        process.exit(1);
+    }
+})
