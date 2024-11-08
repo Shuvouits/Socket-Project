8cf989774df19ebc77b36453b7c68b24a7cd110f
Shuvouits <shuvobhowmik_cse11uits@yahoo.com>
2024-11-08 13:38:34 +0600

    part-2 basic express server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,11 @@
-console.log("hello changes everything");
+import dotenv from "dotenv"
+import app from "./app.js";
+
+//dotenv config
+dotenv.config();
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+    console.log(`server is listening at ${PORT}`);
+});
