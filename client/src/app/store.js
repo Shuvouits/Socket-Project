49d75ef1f10f50adb732f152178f3085da19d88e
Shuvouits <shuvobhowmik_cse11uits@yahoo.com>
2024-11-11 00:04:42 +0600

    part-17 Use redux persist to rehydrate only certain values (smart)

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,11 +1,33 @@
-import {combineReducers, configureStore} from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { persistReducer, persistStore } from "redux-persist"; // Corrected spelling here
+import storage from "redux-persist/lib/storage";
 import userSlice from "../features/userSlice";
+import { createFilter } from "redux-persist-transform-filter"; // Ensure this is also imported
+
+// SavedUserOnlyFilter
+const savedUserOnlyFilter = createFilter("user", ["user"]);
+
+// Persist config
+const persistConfig = {
+    key: "user",
+    storage,
+    whitelist: ["user"],
+    transforms: [savedUserOnlyFilter],
+};
 
 const rootReducer = combineReducers({
     user: userSlice,
 });
 
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
-    reducer: rootReducer,
-    devtools: true,
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: false,
+        }),
+    devTools: true,
 });
+
+export const persistor = persistStore(store); // Corrected spelling here
