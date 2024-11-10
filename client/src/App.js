eab8769f9e293ad6cd227efe1dd7c71bbd5a7861
Shuvouits <shuvobhowmik_cse11uits@yahoo.com>
2024-11-10 22:33:23 +0600

    Part-15 setup basic react route

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   return (
     <div className="dark">
-      <h1 className='bg-white dark:bg-[#000]'>
-        <p className='text-black dark:text-white'>Welcome to frontend</p>
-      </h1>
+
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path='/login' element={<Login/>} />
+          <Route path='/register' element={<Register />} />
+        </Routes>
+      </Router>
       
       
     </div>
