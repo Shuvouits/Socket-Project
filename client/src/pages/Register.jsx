3edccff4f2eaea561e652728131c9c91de42988d
Shuvouits <shuvobhowmik_cse11uits@yahoo.com>
2024-11-15 18:05:37 +0600

    part-25 frontend login and register page setup

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import RegisterForm from '../components/auth/RegisterForm'
 
 function Register() {
   return (
-    <div>Register</div>
+    <div className='h-screen dark:bg-dark_bg_1 flex items-center justify-center py-[19px] overflow-hidden'>
+      <div className='flex w-[1600px] mx-auto h-full'>
+
+        <RegisterForm />
+        
+
+      </div>
+
+    </div>
   )
 }
 
