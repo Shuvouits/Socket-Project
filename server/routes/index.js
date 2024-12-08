ff23db670a0a0a8f7e2b82cc500a2d75c4eaefe2
Shuvouits <shuvobhowmik_cse11uits@yahoo.com>
2024-12-08 12:56:23 +0600

    paert-37 Get User Conversation

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import authRotes from "./auth.route.js";
+import ConversationRoutes from "./conversation.route.js";
 
 
 const router = express.Router();
 
 router.use("/auth", authRotes);
+router.use("/conversation", ConversationRoutes);
 
 export default router;
