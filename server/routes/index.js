68d0a886cd42cf887d6e5862c71574db000f7e47
Shuvouits <shuvobhowmik_cse11uits@yahoo.com>
2024-12-08 13:26:16 +0600

    part-40 Message Backedn

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import authRotes from "./auth.route.js";
 import ConversationRoutes from "./conversation.route.js";
+import MessageRoutes from "./message.route.js";
 
 
 const router = express.Router();
 
 router.use("/auth", authRotes);
 router.use("/conversation", ConversationRoutes);
+router.use("/message", MessageRoutes);
 
 export default router;
